b6ed3e7b4194f6bf813677fb1237ad6eec752436
Michael Ugochukwu <mark.omulu@gmail.com>
2022-06-08 13:47:35 +0100

    Add a store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,5 +1,5 @@
 // @ts-ignore
-import { createStore, combineReducers } from 'react-redux';
+import { createStore, combineReducers } from 'redux';
 import booksReducer from './books/books';
 import categoriesReducer from './categories/categories';
 
@@ -8,4 +8,6 @@ const rootReducer = combineReducers({
   categories: categoriesReducer,
 });
 
-export default createStore(rootReducer);
+const store = createStore(rootReducer);
+
+export default store;
