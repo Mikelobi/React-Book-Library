b2ff8e71c4aef73fd902ec28d8a8315fc5e1d891
Michael Ugochukwu <mark.omulu@gmail.com>
2022-06-10 02:26:41 +0100

    Add style to the navigation bar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,12 +3,15 @@ import Header from './Header';
 import styles from './Navigation.module.css';
 
 function Navigation() {
-  const links = [
+  const mainlinks = [
     {
       id: 1,
       path: '/',
       text: 'Books',
     },
+  ];
+
+  const subLinks = [
     {
       id: 2,
       path: '/categories',
@@ -20,9 +23,20 @@ function Navigation() {
     <nav className={styles.headerNav}>
       <Header />
       <ul className={styles.navigation}>
-        {links.map((link) => (
+        {mainlinks.map((link) => (
           <li key={link.id} className={styles.navLink}>
             <NavLink data-testid={link.id} to={link.path} className={styles.link}>
+              {link.text}
+            </NavLink>
+          </li>
+        ))}
+        {subLinks.map((link) => (
+          <li key={link.id} className={styles.navLink}>
+            <NavLink
+              data-testid={link.id}
+              to={link.path}
+              className={styles.offLink}
+            >
               {link.text}
             </NavLink>
           </li>
